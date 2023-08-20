c2b96bdb15002ca6a8a0aaaee439601aebc44f51
David Pershouse <d3.pershouse@hdr.qut.edu.au>
2023-08-20 17:02:53 +1000

    some changes

--- detection/pretest.sh
+++ detection/pretest.sh
@@ -1,9 +1,15 @@
-python apply_net.py --dataset-dir VOC_DATASET_ROOT --test-dataset voc_completely_annotation_pretest --config-file VOC-Detection/faster-rcnn/UnSniffer.yaml --inference-config Inference/standard_nms.yaml --random-seed 0 --image-corruption-level 0 --visualize 0  --pretest True
+python apply_net.py --dataset-dir ~/datasets/VOS/VOC_DATASET_ROOT --test-dataset voc_completely_annotation_pretest --config-file VOC-Detection/faster-rcnn/UnSniffer-eval.yaml --inference-config Inference/standard_nms.yaml --random-seed 0 --image-corruption-level 0 --visualize 0  --pretest True
+
+echo apply_net done
 
 cd evaluator/
 
-python grid_traverse.py --dataset-dir VOC_DATASET_ROOT --test-dataset voc_completely_annotation_pretest --outputdir ../output/  --config-file VOC-Detection/faster-rcnn/UnSniffer.yaml --inference-config Inference/standard_nms.yaml --random-seed 0 --image-corruption-level 0
+echo doing grid traverse
 
-python energy_thresh.py  --dataset-dir VOC_DATASET_ROOT --config-file VOC-Detection/faster-rcnn/UnSniffer.yaml --inference-config Inference/standard_nms.yaml --image-corruption-level 0
+python grid_traverse.py --dataset-dir ~/datasets/VOS/VOC_DATASET_ROOT --test-dataset voc_completely_annotation_pretest --outputdir ../output/  --config-file VOC-Detection/faster-rcnn/UnSniffer.yaml --inference-config Inference/standard_nms.yaml --random-seed 0 --image-corruption-level 0
+
+echo doing energy traverse
+
+python energy_thresh.py  --dataset-dir ~/datasets/VOS/VOC_DATASET_ROOT --config-file VOC-Detection/faster-rcnn/UnSniffer-eval.yaml --inference-config Inference/standard_nms.yaml --image-corruption-level 0
 
 cd ..
